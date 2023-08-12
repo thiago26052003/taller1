9d6adf44b830086127be942f98b162b224beadce
Santiago <thiago260503@gmail.com>
2023-08-12 11:03:50 -0500

    Agrego la operacion Suma

---
+++ calculadora.py
@@ -0,0 +1,9 @@
+print("Bienvendio a la calculadora que no es calculadora ")
+numero1 = int(input("Ingresa un numero: "))
+numero2 = int(input("Ingresa otro numero: "))
+operador = input("Que operación desea realizer? +, -, %, * : ")
+
+
+if operador == "+":
+    operacion = numero1 + numero2
+    print(operacion)
